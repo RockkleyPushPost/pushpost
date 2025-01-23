c9eeb60b5c25559591350e82c4610e8b05be9cbe
Evgeniy <spkissaal@gmail.com>
2025-01-23 22:56:18 +0500

    Refactoring into microservices

--- internal/storage/migrations/create_friendships_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE friendships
-(
-    user_id    bigint NOT NULL REFERENCES users (id),
-    friend_id  bigint NOT NULL REFERENCES users (id),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (user_id, friend_id)
-);
