8515f809c7c3d94d5b0ca15192641206eb761ff8
Evgeniy <spkissaal@gmail.com>
2025-04-21 03:56:10 +0300

    implementing auth service WIP

--- internal/services/api_gateway/domain/usecase/gateway.go
+++
@@ -1,9 +0,0 @@
-package usecase
-
-type Gateway struct {
-	authServiceURL         string
-	postServiceURL         string
-	messagesServiceURL     string
-	notificationServiceURL string
-	userServiceURL         string
-}
--- internal/services/auth_service/cmd/run.go
+++ internal/services/auth_service/cmd/run.go
@@ -1,8 +1,28 @@
 package main
 
+import (
+	"context"
+	"log"
+	"os"
+	"pushpost/internal/config"
+	lg "pushpost/pkg/logger"
+)
+
+const ServiceName = "auth-service"
+
 func main() {
 	//kafkaBroker := os.Getenv("KAFKA_BROKER")
 	//
 	//usecase := usecase.AuthUseCase{kafkaBroker}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
+	srvLogger := lg.InitLogger(ServiceName)
+
+	cfg, err := config.LoadYamlConfig(os.Getenv("AUTH_SERVICE_CONFIG_PATH"))
+
+	if err != nil {
+
+		log.Fatalf("failed to load gateway service config: %v", err)
+	}
 }
